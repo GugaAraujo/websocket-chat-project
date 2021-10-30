468f44f72ec64ebcbe2123afe17e310ffadb91c0
Gustavo Araújo <gustavo.avk@msn.com>
2021-10-30 18:40:29 -0300

    Fix: Validação de inputs

--- public/script.js
+++ public/script.js
@@ -4,7 +4,8 @@
 const socket = io(location.origin.replace(/^http/, 'ws'))
 
 const botao = document.getElementById("enviar")
-var botao_color = document.getElementById("color")
+let botao_color = document.getElementById("color")
+let msgm_erro_form = document.getElementById("erro_form") 
 botao_color.value = "#000000"
 let cor_escolhida = ""
 
@@ -15,31 +16,38 @@ let cor_escolhida = ""
 
 botao_color.addEventListener("change",()=>{
     cor_escolhida = botao_color.value
-    var author = document.getElementById("input_username").style.color = cor_escolhida;
+    let author = document.getElementById("input_username").style.color = cor_escolhida;
 })
 
 botao.addEventListener("click",(event)=>{
     event.preventDefault()
-    var author = document.getElementById("input_username")
-    var message = document.getElementById("input_message")  
-        var messageObject = {
+    let author = document.getElementById("input_username")
+    let message = document.getElementById("input_message")  
+        let messageObject = {
             author: author.value,
             message: message.value,
             color: cor_escolhida,
         }
 
-        render_mensagem_Enviada(messageObject)
+        if(message.value.length>0&&author.value.length>0){
+            msgm_erro_form.style.display = "none"
+            render_mensagem_Enviada(messageObject)
 
-        socket.emit('sendMessage', messageObject)
-        message.value =""
+            socket.emit('sendMessage', messageObject)
+            message.value =""
+        }else{
+            msgm_erro_form.style.display = "block"
+        }
+
+
 })
 
 window.document.addEventListener('keyup', function(event){
     if (event.key == 13|| event.key === "Enter") {
         event.preventDefault()
-        var author = document.getElementById("input_username")
-        var message = document.getElementById("input_message")  
-            var messageObject = {
+        let author = document.getElementById("input_username")
+        let message = document.getElementById("input_message")  
+            let messageObject = {
                 author: author.value,
                 message: message.value,
                 color: cor_escolhida,
@@ -81,7 +89,7 @@ function render_mensagem_recebida(message){
     let dataAtual = new Date();
 let hora = dataAtual.toLocaleTimeString()
     $('.caixa_chat').append(`<div class="message"><span style="color:#a2a2a2;font-size:12px">${hora} - </span><strong><span style="color:${message.color}">${message.author}</span></strong>: <span style="padding-left:10px">${message.message}</span></div>`)
-    var myDiv = document.getElementById("caixa_chat");
+    let myDiv = document.getElementById("caixa_chat");
     myDiv.scrollTop = myDiv.scrollHeight;
 }
 
@@ -90,7 +98,7 @@ function render_mensagem_Enviada(message){
     let dataAtual = new Date();
 let hora = dataAtual.toLocaleTimeString()
     $('.caixa_chat').append(`<div class="message"><span style="color:#a2a2a2;font-size:12px">${hora} - </span><strong><span style="color:${cor_escolhida}">${message.author}</span></strong>: <span style="padding-left:10px">${message.message}</span></div>`)
-var myDiv = document.getElementById("caixa_chat");
+let myDiv = document.getElementById("caixa_chat");
     myDiv.scrollTop = myDiv.scrollHeight;
 }
 
