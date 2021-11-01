92aec30518871f26ba66ac6613a1fc7b7627071f
Gustavo Araújo <gustavo.avk@msn.com>
2021-10-31 23:21:54 -0300

    Refactor: Optimizando leitura do código, utilizando funções.

--- public/script.js
+++ public/script.js
@@ -1,25 +1,78 @@
-
-
-
 const socket = io(location.origin.replace(/^http/, 'ws'))
 
-const botao = document.getElementById("enviar")
-let botao_color = document.getElementById("color")
+const btn_enviar = document.getElementById("enviar")
 let msgm_erro_form = document.getElementById("erro_form") 
+
+let botao_color = document.getElementById("color")
 botao_color.value = "#000000"
 let cor_escolhida = ""
 
+let author = document.getElementById("input_username")
+author.value = ""
+
+let message = document.getElementById("input_message")
+message.value = ""
 
 
-
-
-
+//Atribuindo nova cor de nick, que será enviada no Objeto MessageObject
 botao_color.addEventListener("change",()=>{
     cor_escolhida = botao_color.value
-    let author = document.getElementById("input_username").style.color = cor_escolhida;
+    author.style.color = cor_escolhida;
 })
 
-botao.addEventListener("click",(event)=>{
+
+//Enviando mensagens através de evento de ENTER e Click
+btn_enviar.addEventListener("click",(event)=>{
+    enviar_mensagem(event)
+})
+
+window.document.addEventListener('keyup', function(event){
+    if (event.key == 13|| event.key === "Enter") {
+        enviar_mensagem(event)
+      }
+});
+
+
+//Recebimento de mensagens do servidor
+socket.on("entrada", function(saudacao){
+    $('.caixa_chat').append(`<div class="saudacao"><span>${saudacao}</span></div>`)
+})
+
+socket.on("aviso", function(aviso){
+    $('.caixa_chat').append(`<div class="aviso"><span>${aviso}</span></div>`)
+})
+
+socket.on("mensagensAnteriores", function(messages){
+    for (message of messages){
+        render_mensagem(message,message.color)
+    }
+})
+
+socket.on("receivedMessage", function(message){
+    render_mensagem(message,message.color)
+})
+
+
+
+
+
+
+
+// ============= Funções declaradas =================
+
+
+// O primeiro parametro se refere ao MessageObject
+//O segundo será: Ou cor escolhida, no caso de envio de mensagem, ou a cor de quem enviou a mensagem
+function render_mensagem(message,cor_do_nick){
+    let dataAtual = new Date();
+let hora = dataAtual.toLocaleTimeString()
+    $('.caixa_chat').append(`<div class="message"><span style="color:#a2a2a2;font-size:12px">${hora} - </span><strong><span style="color:${cor_do_nick}">${message.author}</span></strong>: <span style="padding-left:10px">${message.message}</span></div>`)
+let myDiv = document.getElementById("caixa_chat");
+    myDiv.scrollTop = myDiv.scrollHeight;
+}
+
+//Verificando os imputs estão preenchidos antes de renderizar na tela e enviar o MessageObject ao servidor.
+function enviar_mensagem(event){
     event.preventDefault()
     let author = document.getElementById("input_username")
     let message = document.getElementById("input_message")  
@@ -29,86 +82,14 @@ botao.addEventListener("click",(event)=>{
             color: cor_escolhida,
         }
 
+ 
         if(message.value.length>0&&author.value.length>0){
             msgm_erro_form.style.display = "none"
-            render_mensagem_Enviada(messageObject)
+            render_mensagem(messageObject,cor_escolhida)
 
             socket.emit('sendMessage', messageObject)
             message.value =""
         }else{
             msgm_erro_form.style.display = "block"
         }
-
-
-})
-
-window.document.addEventListener('keyup', function(event){
-    if (event.key == 13|| event.key === "Enter") {
-        event.preventDefault()
-        let author = document.getElementById("input_username")
-        let message = document.getElementById("input_message")  
-            let messageObject = {
-                author: author.value,
-                message: message.value,
-                color: cor_escolhida,
-            }
-    
-     
-            if(message.value.length>0&&author.value.length>0){
-                msgm_erro_form.style.display = "none"
-                render_mensagem_Enviada(messageObject)
-    
-                socket.emit('sendMessage', messageObject)
-                message.value =""
-            }else{
-                msgm_erro_form.style.display = "block"
-            }
-      }
-});
-
-
-socket.on("entrada", function(saudacao,aviso){
-    $('.caixa_chat').append(`<div class="saudacao"><span>${saudacao}</span></div>`)
-})
-socket.on("aviso", function(aviso){
-    $('.caixa_chat').append(`<div class="aviso"><span>${aviso}</span></div>`)
-})
-
-socket.on("mensagensAnteriores", function(messages){
-    for (message of messages){
-        render_mensagem_recebida(message)
-    }
-})
-
-socket.on("testa_conexao",(testa_conexao)=>{
-    socket.emit("resposta_conexao",testa_conexao)
-})
-
-socket.on("receivedMessage", function(message){
-    render_mensagem_recebida(message)
-})
-
-
-
-
-
-function render_mensagem_recebida(message){
-    let dataAtual = new Date();
-let hora = dataAtual.toLocaleTimeString()
-    $('.caixa_chat').append(`<div class="message"><span style="color:#a2a2a2;font-size:12px">${hora} - </span><strong><span style="color:${message.color}">${message.author}</span></strong>: <span style="padding-left:10px">${message.message}</span></div>`)
-    let myDiv = document.getElementById("caixa_chat");
-    myDiv.scrollTop = myDiv.scrollHeight;
 }
-
-
-function render_mensagem_Enviada(message){
-    let dataAtual = new Date();
-let hora = dataAtual.toLocaleTimeString()
-    $('.caixa_chat').append(`<div class="message"><span style="color:#a2a2a2;font-size:12px">${hora} - </span><strong><span style="color:${cor_escolhida}">${message.author}</span></strong>: <span style="padding-left:10px">${message.message}</span></div>`)
-let myDiv = document.getElementById("caixa_chat");
-    myDiv.scrollTop = myDiv.scrollHeight;
-}
-
-
-
-
--- server.js
+++ server.js
@@ -18,73 +18,63 @@ app.use("/",(req, res)=>{
     res.render("index.html")
 })
 
-var messages = [];
-const intervalo_remocacao_frases = 120000
 
+const intervalo_remover_frases = 120000
+
+let messages = [];
 let total_online = 0
 let record_online = 0
 
 const saudacao = `Seja bem vindo! &#128516`
-const aviso = `A cada ${intervalo_remocacao_frases/60000} minutos, uma frase é removida no histórico do chat. Nada ficará gravado por muito tempo.`
+const aviso = `A cada ${intervalo_remover_frases/60000} minutos, uma frase é removida no histórico do chat. Nada ficará gravado por muito tempo.`
 
 
 io.on("connection",socket =>{
+
     //contando total de usuários ativos e conferindo se ultrapasssa o record
     total_online ++
     if(total_online>record_online){record_online=total_online}
 
 
-    console.log(`
-    O dispositivo ${socket.id} ainda está online. 
-    Total online: ${total_online}. 
-    Recorde de usuários simultâneos: ${record_online}
-     `)
-
-
-    console.log(`Socket conectado:${socket.id}`)
-
     //aviso de entrada de novo usuario
     let entrou_usuario = `${socket.id} entrou na sala. Total online: ${total_online}. Recorde online: ${record_online}`
+    console.log(`
+${entrou_usuario}
+         `)
     socket.broadcast.emit("aviso",entrou_usuario)
 
 
+    //Enviando histórico, saudação e aviso sobre o histórico ao usuário recém chegado
     socket.emit('mensagensAnteriores',messages)
     socket.emit('entrada',saudacao)
-    setTimeout(()=>{
-        socket.emit('aviso',aviso)
-    },2000)
+    setTimeout(() => socket.emit('aviso',aviso),2000)
 
+
+    //Ao receber mensagem, enviamos ao histórico temporário, exibimos no serivor e reenviamos a todos
     socket.on('sendMessage', data =>{
         messages.push(data)
         console.log(messages)
         socket.broadcast.emit('receivedMessage',data)
     })
 
-    socket.on('disconnect', function(data) {
+    //No momento de desconexão do usuário, decrescemos nossa contagem de usuários online...
+    socket.on('disconnect', function() {
         total_online = total_online -1
 
 
-          //aviso de entrada de novo usuario
-    let saiu_usuario = `${socket.id} saiu da sala. Total online: ${total_online}. Recorde online: ${record_online}`
-    socket.broadcast.emit("aviso",saiu_usuario)
+        // ... Então avisamos a todos sobre a saída do usuário
+        let saiu_usuario = `${socket.id} saiu da sala. Total online: ${total_online}. Recorde online: ${record_online}`
+        socket.broadcast.emit("aviso",saiu_usuario)
 
-
+        //... exibimos no console.
         console.log(`
-O dispositivo ${socket.id} saiu. 
-Total online: ${total_online}. 
-Recorde de usuários simultâneos: ${record_online}
+${saiu_usuario}
          `)
 
     });
 })
 
-server.listen(porta, ()=>{
+server.listen(porta,()=>console.log(`Conectado!`))
 
-    console.log(`Conectado!`)
-    
-})
-
-
-setInterval(() => {
-    messages.shift()
-}, intervalo_remocacao_frases);
+//Removendo do topo do histórico uma frase a cada intervalo de tempo pré determinado
+setInterval(() => messages.shift(), intervalo_remover_frases);
