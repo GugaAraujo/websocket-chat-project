9d8982b78518037acc5e40d46506d580a747e4a1
Gustavo Araújo <gustavo.avk@msn.com>
2021-11-02 19:31:07 -0300

    add: Modal com form de usuario

--- public/script.js
+++ public/script.js
@@ -1,20 +1,16 @@
-const socket = io(location.origin.replace(/^http/, 'ws'))
 
-let total_online = document.getElementById("total_online")
-let record_online = document.getElementById("record_online")
 
-const btn_enviar = document.getElementById("enviar")
-let msgm_erro_form = document.getElementById("erro_form") 
+//formulário do Modal
+let campo_nome = document.getElementById("input_username")
+let btn_username = document.getElementById("btn_username")
 
+//seletor de cor
 let botao_color = document.getElementById("color")
 botao_color.value = "#000000"
 let cor_escolhida = ""
 
 let author = document.getElementById("input_username")
-author.value = ""
-
-let message = document.getElementById("input_message")
-message.value = ""
+author.value=""
 
 
 //Atribuindo nova cor de nick, que será enviada no Objeto MessageObject
@@ -24,6 +20,97 @@ botao_color.addEventListener("change",()=>{
 })
 
 
+
+// Get the modal
+let modal = document.getElementById("modal");
+
+
+
+
+// Get the button that opens the modal
+let btn = document.getElementById("btn_username");
+btn.addEventListener("click",(event)=>{
+    event.preventDefault()
+    modal.style.display = "block";
+});
+
+// Get the <span> element that closes the modal
+let span = document.getElementById("close")
+span.addEventListener("click",(event)=>{
+    if(campo_nome.value.length>0){
+    modal.style.display = "none";
+    insere_nome(campo_nome.value)
+}
+});
+
+
+
+
+
+
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal&&campo_nome.value.length>0) {
+    modal.style.display = "none";
+    insere_nome(campo_nome.value)   
+  }
+} 
+
+window.document.addEventListener('keyup', function(event){
+    if (event.key == 13|| event.key === "Enter") {
+        if(event.target == campo_nome&&campo_nome.value.length>0){
+            modal.style.display = "none";
+            insere_nome(campo_nome.value)
+        }
+      }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const socket = io(location.origin.replace(/^http/, 'ws'))
+
+let total_online = document.getElementById("total_online")
+let record_online = document.getElementById("record_online")
+
+const btn_enviar = document.getElementById("enviar")
+let msgm_erro_form = document.getElementById("erro_form") 
+
+
+
+
+
+let message = document.getElementById("input_message")
+message.value = ""
+
+
+
+
+
 //Enviando mensagens através de evento de ENTER e Click
 btn_enviar.addEventListener("click",(event)=>{
     enviar_mensagem(event)
@@ -31,7 +118,9 @@ btn_enviar.addEventListener("click",(event)=>{
 
 window.document.addEventListener('keyup', function(event){
     if (event.key == 13|| event.key === "Enter") {
-        enviar_mensagem(event)
+        if(event.target==message){
+            enviar_mensagem(event)
+        }
       }
 });
 
