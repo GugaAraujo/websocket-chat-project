4560c8bc2cd7cae378bc601e428c48fd55bf14cf
Gustavo Araújo <gustavo.avk@msn.com>
2022-05-19 21:20:58 -0300

    feature(utils): new function to facilitate console.log

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,9 @@
-export function getTime(): string {
+function getTime(): string {
     return new Date().toLocaleTimeString('pt-BR', {timeZone: 'America/Sao_Paulo'})
 }
+
+function log(...args): void{
+    console.log(...args)
+}
+
+export { getTime, log }
